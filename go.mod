ca15c1385ddbe2a7615aa0ca020e00be23947c95
MAOZ <maooz4426@gmail.com>
2025-06-05 19:35:52 +0900

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maooz4426/hello-test
+
+go 1.24.1
---
+++ hello.go
@@ -0,0 +1,15 @@
+package hello
+
+import (
+	"errors"
+	"fmt"
+)
+
+func Hello(name string) (string, error) {
+	if name != "" {
+		return "", errors.New("empty name")
+	}
+
+	message := fmt.Sprintf("Hello %s", name)
+	return message, nil
+}
